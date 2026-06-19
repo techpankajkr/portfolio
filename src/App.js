9e10ca8c3de4cccddc5ec04d10833e7a1136c174
techpankajkr <tech.pankajkr1@gmail.com>
2026-06-19 18:35:25 +0530

    Added GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Projects from "./Pages/Projects";
 import Technologies from "./Pages/Technologies";
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
